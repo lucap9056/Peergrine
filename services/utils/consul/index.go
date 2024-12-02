ba472d78c07f570087010a9406f840f700f3342d
lucap9056 <jim12366219@gmail.com>
2024-12-02 14:34:29 +0800

    feat(consul): add connection retry mechanism during initialization
    - Implemented retry logic for handling connection initialization failures in Consul client

--- services/utils/consul/index.go
+++ services/utils/consul/index.go
@@ -1,13 +1,31 @@
 package consulclient
 
 import (
+	"log"
+	"time"
+
 	"github.com/hashicorp/consul/api"
 )
 
-func New(addr string) (*api.Client, error) {
+const _MAX_RETRY_ATTEMPTS = 10
+const _RETRY_INTERVAL_TIME = time.Second * 5
 
+func New(addr string) (*api.Client, error) {
 	config := api.DefaultConfig()
 	config.Address = addr
 
-	return api.NewClient(config)
+	var client *api.Client
+	var err error
+
+	for attempt := 1; attempt <= _MAX_RETRY_ATTEMPTS; attempt++ {
+		client, err = api.NewClient(config)
+		if err == nil {
+			return client, nil
+		}
+
+		log.Printf("Attempt %d: Failed to create Consul client: %v", attempt, err)
+		time.Sleep(_RETRY_INTERVAL_TIME)
+	}
+
+	return nil, err
 }
